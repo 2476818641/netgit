b185434b09c66459ab4bf53f48330c890a6a34c8
2476818641 <68951020+2476818641@users.noreply.github.com>
2025-11-13 15:40:40 +0800

    Update generate-home.js

--- netlify/generate-home.js
+++ netlify/generate-home.js
@@ -1,7 +1,7 @@
 // netlify/generate-home.js
 import fs from 'fs';
 import path from 'path';
-import { generateStaticHomePage, NETLIFY_HOME_DOMAIN, CF_PAGES_HOME_DOMAIN } from '../../shared/config.js';
+import { generateStaticHomePage, NETLIFY_HOME_DOMAIN, CF_PAGES_HOME_DOMAIN } from '../shared/config.js';
 
 // process.cwd() 获取当前工作目录，这里假设在 netlify 目录下运行
 const htmlContent = generateStaticHomePage('Netlify', NETLIFY_HOME_DOMAIN, CF_PAGES_HOME_DOMAIN);
